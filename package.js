8b3e3acd1037909485b5e2d4d0c43e017189defd
Matteo De Micheli <mat.demicheli@gmail.com>
2015-11-04 16:44:40 +0100

    Fix no configuration fail

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sycro:blocks',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Easily create full page blocks with fullpage.js and Blaze',
   // URL to the Git repository containing the source code for this package.
